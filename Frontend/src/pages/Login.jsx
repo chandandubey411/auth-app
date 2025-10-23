4980f543dac6ebaf1f2ebf0d5ca48a4dd95d13ce
chandandubey411 <dubeychandan411@gmail.com>
2025-10-23 12:57:31 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
   }
 
   try {
-    const url = "https://auth-app-backend-hupf.onrender.com/login";
+    const url = "https://auth-app-backend-hupf.onrender.com/auth/login";
     const response = await fetch(url, {
       method: "POST",
       headers: {
